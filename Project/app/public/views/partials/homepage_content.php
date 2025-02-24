f38d1f40172e7519cc404b3158bf8e0d4df089bc
tarikkeser <ahmettariknl@gmail.com>
2025-02-24 11:49:09 +0100

    uncommented

--- Project/app/public/views/partials/homepage_content.php
+++ Project/app/public/views/partials/homepage_content.php
@@ -5,14 +5,14 @@ Then this file will be included in the index.php file (which is in pages.)-->
 
 <?php
 // top picture
-//require(__DIR__ . "/../partials/slideshow.php");
+require(__DIR__ . "/../partials/slideshow.php");
 
 // welcoming part
-// require(__DIR__ . "/../partials/welcoming.php");
+require(__DIR__ . "/../partials/welcoming.php");
 
 // event dates
 
 // map
 
 // faq 
-// require(__DIR__ . "/../partials/faq.php");
+require(__DIR__ . "/../partials/faq.php");
