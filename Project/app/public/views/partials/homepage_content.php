6a331a3a96fa4a66e815a958878b39d754ea6311
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-02-24 11:39:02 +0100

    Revert "Slideshow 1.1"
    This reverts commit a17f4e0b01afb6d2a2f01c233c20650343e011cd.

--- Project/app/public/views/partials/homepage_content.php
+++ Project/app/public/views/partials/homepage_content.php
@@ -1,3 +1,4 @@
+<h1>Homepage</h1>
 <?php require(__DIR__ . "/../partials/slideshow.php"); ?>
 <p>
     <a href="users">users</a>
--- Project/app/public/views/partials/slideshow.php
+++ Project/app/public/views/partials/slideshow.php
@@ -1,16 +1,32 @@
 
+<style>
+    body {font-family:Arial, Helvetica, sans-serif; font-size:12px;}
+    
+    .fadein {
+        position:relative; height:332px; width:500px; margin:0 auto;
+        background: #ebebeb;
+        padding: 10px;
+    }
+    .fadein img{
+            position:absolute;
+            width: calc(96%);
+        height: calc(94%);
+        object-fit: scale-down;
+    }
+</style>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script>
     $(function(){
             $('.fadein img:gt(0)').hide();
             setInterval(function(){
             $('.fadein :first-child').fadeOut().next('img').fadeIn().end
             ().appendTo('.fadein');
-            }, 9500);
+            }, 3000);
     });
 </script>
-<div class="fadein p-0">
+<div class="fadein">
     <?php
-    // dsplay images from directory
+    // display images from directory
     // directory path
     $dir = "assets/Sidebar/";
     
@@ -19,6 +35,6 @@
             if(in_array($img,array('.','..')))
             continue;
     ?>
-    <img src="<?php echo $dir.$img ?>" alt="<?php echo $img ?> " class = "image-fluid";>
+    <img src="<?php echo $dir.$img ?>" alt="<?php echo $img ?>">
     <?php endforeach; ?>
 </div>
