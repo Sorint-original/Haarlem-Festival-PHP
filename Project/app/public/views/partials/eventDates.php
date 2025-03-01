521fa76455fbcda091a920cad6378d3b41f358f7
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-01 21:16:39 +0100

    switch dates made possible

--- Project/app/public/views/partials/eventDates.php
+++ Project/app/public/views/partials/eventDates.php
@@ -1,6 +1,32 @@
 
 
-<section class ="eventDates container-fluid m-5 p-0">
-    <menu class = "d-flex flex-row d-flex justify-content-evenly m-0 w-100">
+<section id="eventDates" class ="container-fluid m-5 p-0">
+    <menu class = "d-flex flex-row d-flex justify-content-between px-5 pt-5 m-0 w-100">
+        <button type="button" class ="btn eventDate H2 current">Thursday Jul 24</button >
+        <button type="button"  class ="btn eventDate H2">Friday Jul 25</button >
+        <button type="button"  class ="btn eventDate H2">Saturday Jul 26</button >
+        <button type="button"  class ="btn eventDate H2">Sunday Jul 27</button >
     </menu>
 </section>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+    const DateButtons = document.getElementsByClassName('eventDate');
+    const DateSection = document.getElementById('eventDates');
+
+    const changeeventDates = (image,clickedButton) => {
+        Array.from(DateButtons).forEach(button => {
+            button.classList.remove('current');
+        });
+        clickedButton.classList.add('current');
+        DateSection.style.backgroundImage = `url(assets/images/${image}.png)`;
+    };
+
+    const specificParameters = ['SchedualeDay1', 'SchedualeDay2', 'SchedualeDay3', 'SchedualeDay4'];
+
+    Array.from(DateButtons).forEach(button => {
+        const img = specificParameters[Array.from(DateButtons).indexOf(button)];
+        button.addEventListener('click',  () => changeeventDates(img,button));
+    });
+});
+</script>
