50ba7ab75a6294286b3fc7f354a60ff09b4f0e04
lai67 <144331843+lai67@users.noreply.github.com>
2025-03-03 09:38:05 +0100

    login
    login

--- Project/app/public/index.php
+++ Project/app/public/index.php
@@ -23,6 +23,9 @@ require_once(__DIR__ . "/lib/Route.php");
  */
 require_once(__DIR__ . "/routes/index.php");
 require_once(__DIR__ . "/routes/user.php");
+require_once(__DIR__ . "/routes/login.php");
+require_once(__DIR__ . "/routes/yummy_main.php");
+
 
 // Start the router, enabling handling requests
 Route::run();
---
+++ Project/app/public/routes/yummy_main.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::add('/yummy', function () {
+    // homepage is simply loading a static page
+    // view the user routes for example following the MVC pattern
+    require(__DIR__ . "/../views/pages/yummy_main.php");
+});
