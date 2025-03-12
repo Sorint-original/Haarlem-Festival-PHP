b85f23313e9c14b4d4b460c9e72d7efd2190a731
Big-B01 <144338863+Big-B01@users.noreply.github.com>
2025-03-12 09:50:53 +0100

    Updated slideshow updates
    updated pictures, allignments and text

--- Project/app/public/views/partials/slideshow.php
+++ Project/app/public/views/partials/slideshow.php
@@ -1,40 +1,33 @@
 
-<style>
-    body {font-family:Arial, Helvetica, sans-serif; font-size:12px;}
-    
-    .fadein {
-        position:relative; height:332px; width:500px; margin:0 auto;
-        background: #ebebeb;
-        padding: 10px;
-    }
-    .fadein img{
-            position:absolute;
-            width: calc(96%);
-        height: calc(94%);
-        object-fit: scale-down;
-    }
-</style>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script>
     $(function(){
             $('.fadein img:gt(0)').hide();
             setInterval(function(){
             $('.fadein :first-child').fadeOut().next('img').fadeIn().end
             ().appendTo('.fadein');
-            }, 3000);
+            }, 10000);
     });
 </script>
-<div class="fadein">
+
+<div class="fadein w-100">
     <?php
     // display images from directory
     // directory path
     $dir = "assets/Sidebar/";
-    
     $scan_dir = scandir($dir);
     foreach($scan_dir as $img):
             if(in_array($img,array('.','..')))
             continue;
     ?>
-    <img src="<?php echo $dir.$img ?>" alt="<?php echo $img ?>">
+    <img src="<?php echo $dir.$img ?>" alt="<?php echo $img ?>" class= "h-100 w-100">
     <?php endforeach; ?>
+
+</div> 
+
+<!-- Overlay Image and Text -->
+<div class="overlay">
+    <div class="overlay-content">
+        <img src="assets/favicons/logo.png" alt="Haarlem Festival Logo" class="overlay-image">
+        <p class="overlay-text">Haarlem Festival</p>
+    </div>
 </div>
