c2d2b21da5a94652c71b38402d0a813e4ce65849
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-15 15:59:27 +0100

    switching day

--- Project/app/public/assets/js/JazzPage.js
+++ Project/app/public/assets/js/JazzPage.js
@@ -1,15 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const DaySection = document.getElementById('jazz-day');
     const Cday = document.getElementById('CurrentDay');
+    const PrevButton =document.getElementById('PrevDay');
+    const NextButton =document.getElementById('NextDay');
+
     const dayBackgrounds = ['D0','D1','D2','D3',];
     const days = ['THURSDAY JUL 24', 'FRIDAY JUL 25','SATURDAY JUL 26','SUNDAY JUL 27'];
-    const CurrentDay = 0;
+    var CurrentDay = 0;
 
 
     const SetDay = () =>{
         DaySection.style.backgroundImage = `url(assets/images/jazz/${dayBackgrounds[CurrentDay]}.png)`;
         Cday.innerHTML =  days[CurrentDay];
     }
+
+    const NextDay =() =>{
+        CurrentDay += 1;
+        if(CurrentDay == 4){
+            CurrentDay=0;
+        }
+        SetDay();
+    }
+
+    const PrevDay =() =>{
+        CurrentDay -= 1;
+        if(CurrentDay == -1){
+            CurrentDay=3;
+        }
+        SetDay();
+    }
+
+    PrevButton.addEventListener('click',  () => PrevDay());
+    NextButton.addEventListener('click',  () => NextDay());
     
     SetDay();
 });
--- Project/app/public/views/partials/jazz-partials/jazz_schedule.php
+++ Project/app/public/views/partials/jazz-partials/jazz_schedule.php
@@ -1,9 +1,9 @@
 
 <section class="jazz-Section my-5">
     <menu id= "jazz-day" class = " w-100 p-0 m-0 d-flex flex-row justify-content-between align-items-center"> 
-        <button></button>
-        <header id = "CurrentDay" class ="display-1 text-center"></header>
-        <button></button>
+        <button type="button"  id="PrevDay"><img src = "/assets/images/jazz/Prev.png"></button>
+        <header type="button"  id = "CurrentDay" class ="display-1 text-center"></header>
+        <button type="button"  id="NextDay"><img src = "/assets/images/jazz/Next.png"></button>
     </menu>
 
 </section>
