96e237a699a63a859cee5023268ec9dc9b9f62fc
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-16 11:29:39 +0100

    asset link fix

--- Project/app/public/views/pages/jazzBand.php
+++ Project/app/public/views/pages/jazzBand.php
@@ -1,1 +1,6 @@
-SUNT AICI
+<?php
+
+$page_type = "jazz"; #this way we can set which color backround is set in the header based on the page type
+$fontColoring = 'fontCdark'; #this is how we define how the font should be colored based on the background
+
+require(__DIR__ . "/../partials/header.php");
