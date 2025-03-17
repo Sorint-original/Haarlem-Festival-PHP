51c666b45b978a077756a0dc71940a9cf0764a3a
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-17 16:51:05 +0100

    slide fix

--- Project/app/public/views/partials/slideshow.php
+++ Project/app/public/views/partials/slideshow.php
@@ -1,13 +1,18 @@
 
 <script>
     $(function(){
-                $('.fadein img:gt(0)').hide(); // Hide all images except the first one initially
+        // Check if there is more than one image in the slideshow
+        if ($('.fadein img').length > 1) {
+            $('.fadein img:gt(0)').hide(); // Hide all images except the first one initially
             setInterval(function(){
                 $('.fadein :first-child').fadeOut().next('img').fadeIn().end()
                 .appendTo('.fadein'); // Move the first image to the end after it fades out
             }, 10000); // Change images every 10 seconds
+        }
+        // If there's only one image, do nothing (it will remain visible)
     });
 </script>
+
 <div class="header-content w-100">
 <div class="fadein w-100">
     <?php
@@ -23,7 +28,7 @@
         <?php if ($page_type == "homepage") {?>
         <img src="assets/favicons/logo.png" alt="Haarlem Festival Logo" class="overlay-image">
         <?php } ?>
-        <p class="overlay-text display-1 font-weight-bold <?php echo $page_type; ?>" ><?php echo $title; ?></p>
+        <p class="overlay-text font-weight-bold <?php echo $page_type; ?>" ><?php echo $title; ?></p>
     </div>
 </div>
 </div>
