46faa64cc8f5560751876311097e14429adf5888
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-17 14:31:21 +0100

    fixed homepage content script

--- Project/app/public/views/partials/footer.php
+++ Project/app/public/views/partials/footer.php
@@ -84,7 +84,6 @@
 </section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/js/HomepageContent.js"></script>
 </body>
 </html>
 
