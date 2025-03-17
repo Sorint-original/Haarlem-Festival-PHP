179dfcb571a6c854b82a1da1343f2eab7221c0de
tarikkeser <ahmettariknl@gmail.com>
2025-03-17 14:15:55 +0100

    initial

--- Project/app/public/views/pages/admin/admin-users.php
+++ Project/app/public/views/pages/admin/admin-users.php
@@ -1,5 +1,7 @@
 <?php
 
-//This page is created to be able to edit homepage content.
+//This page is created to be able to edit users.
 
 require(__DIR__ . "/../../partials/admin-partials/admin-navbar.php");
+
+require(__DIR__ . "/../../partials/admin-partials/admin-users-content.php");
