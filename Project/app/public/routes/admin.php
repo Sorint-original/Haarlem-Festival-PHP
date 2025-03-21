e503cd775a4cd2cadd9bf9c2062ab9521d18d976
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-21 09:30:11 +0100

    start admin

--- Project/app/public/routes/admin.php
+++ Project/app/public/routes/admin.php
@@ -15,8 +15,20 @@ Route::add('/admin/admin-homepage', function () {
     $controller = new AdminController();
     $controller->getPageContent();
 }, 'get');
+
 // update admin homepage data
 Route::add('/admin/admin-homepage', function () {
+    $controller = new AdminController();
+    $controller->updatePage();
+}, 'post');
+
+// get admin homepage data
+Route::add('/admin/admin-jazz', function () {
+    require(__DIR__ . "/../views/pages/admin/admin-jazz.php");
+}, 'get');
+
+// update admin homepage data
+Route::add('/admin/admin-jazz', function () {
     $controller = new AdminController();
     $controller->updatePage();
 }, 'post');
---
+++ Project/app/public/views/pages/admin/admin-jazz.php
@@ -0,0 +1,7 @@
+<?php
+
+//This page is created to be able to edit jazz content.
+
+require(__DIR__ . "/../../partials/admin-partials/admin-navbar.php");
+
+require(__DIR__ . "/../../partials/admin-partials/admin-jazz-content.php");
