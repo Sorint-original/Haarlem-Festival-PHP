f855e405751c065cd8dc8715d5baec170ce92898
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-03-21 11:41:41 +0100

    flexibility implementation in page admin edit

--- Project/app/public/views/pages/admin/admin-jazz.php
+++ Project/app/public/views/pages/admin/admin-jazz.php
@@ -1,7 +1,9 @@
-<?php
+
 
 //This page is created to be able to edit jazz content.
 
-require(__DIR__ . "/../../partials/admin-partials/admin-navbar.php");
+<?php require(__DIR__ . "/../../partials/admin-partials/admin-navbar.php"); ?>
 
-require(__DIR__ . "/../../partials/admin-partials/admin-jazz-content.php");
+<script src="/assets/js/Admin/Admin-pageEdit.js"></script>
+
+<?php require(__DIR__ . "/../../partials/admin-partials/admin-jazz-content.php");?>
